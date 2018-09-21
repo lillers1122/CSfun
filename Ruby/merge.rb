d10f50b7a93e51e8a560548b8de77fd28eaa7aff
Lily Sky <lsky@ext.uber.com>
2018-09-21 12:11:49 -0700

    merge sort added to merge 2 sorted arrays

--- Ruby/merge.rb
+++ Ruby/merge.rb
@@ -1,11 +1,9 @@
-#merge sort takes in 2 sorted arrays
-
-def merge(arr1,arr2)
+def mergeSorted(arr1,arr2)
   arr3 = []
   i = 0
   j = 0
   while i < arr1.length && j < arr2.length
-      if arr1[i] < arr2[j]
+      if arr1[i] <= arr2[j]
         arr3 << arr1[i]
         i += 1
       elsif arr2[j] < arr1[i]
@@ -23,10 +21,23 @@ def merge(arr1,arr2)
     arr3 << arr2[j]
     j += 1
   end
-  p arr3
+  return arr3
 end
 
-array1 = [1,2,4]
-array2 = [3,5,6,7]
+def mergeSort(arr)
+  if arr.length < 2
+    return arr
+  else
+    mid = (arr.length)/2
+    l = arr[0,mid]
+    r = arr[mid,arr.length]
+    return mergeSorted(mergeSort(l), mergeSort(r))
+  end
+end
 
-merge(array1,array2)
+myArray = [4,3,7,8,1]
+p "Merge sort: " + myArray.to_s + " => " + mergeSort(myArray).to_s
+
+array1 = [1,2,3]
+array2 = [2,3,4]
+p "Merge 2 sorted arrays: " + array1.to_s + " + " + array2.to_s + " => " + mergeSorted(array1,array2).to_s
