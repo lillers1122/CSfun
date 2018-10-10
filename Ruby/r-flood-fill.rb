2cc436da7f11bc445a9d264aab72df3638cfae67
Lily Sky <lsky@ext.uber.com>
2018-10-10 14:08:51 -0700

    recursive flood-fill

---
+++ Ruby/r-flood-fill.rb
@@ -0,0 +1,22 @@
+myMatrix = [['z','z','z','y'],
+            ['z','y','y','z'],
+            ['z','y','z','z'],
+            ['z','y','y','y']]
+
+def flood_fill(matrix, coor1, coor2, v)
+  previous = matrix[coor1][coor2]
+  matrix[coor1][coor2] = v
+
+  directions = [[0,1],[0,-1],[1,0],[-1,0],] #R,L,B,T
+
+  directions.each do |coords|
+    coords[0] += coor1
+    coords[1] += coor2
+    if matrix[coords[0]][coords[1]] == previous
+      flood_fill(matrix,coords[0],coords[1],v)
+    end
+  end
+  matrix
+end
+p myMatrix
+p flood_fill(myMatrix,2,2,"a")
